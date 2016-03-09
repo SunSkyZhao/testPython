12ce284308746d3e89c6b85ed6a3a4fccb67acb0
SunSkyZhao <dingchao438@qq.com>
2016-03-09 17:30:00 +0800

    rewrite url

--- learnFlask/hello.py
+++ learnFlask/hello.py
@@ -1,6 +1,9 @@
+# 导入了flask类
 from flask import Flask
+# 创建一个该类的实例
 app = Flask(__name__)
 
+# route装饰器确定触发的url
 @app.route('/')
 def index():
     return "index page!"
@@ -18,5 +21,7 @@ def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
 
+# 确保该服务器只在python解释器执行的时候才会运行
 if __name__ == '__main__':
+    # run函数运行
     app.run()
